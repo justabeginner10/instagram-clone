dfe236562979ae66acfde96d163867752403f39e
Aditya <adityaraj60303@gmail.com>
2024-11-01 17:33:56 +0530

    Initial Setup

---
+++ Instagram-Clone/Instagram/Home/View/IgHomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Aditya on 01/11/24.
+//
+
+import UIKit
+import AsyncDisplayKit
+import RxSwift
+import IGListKit
+import SnapKit
+
+class IgHomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
